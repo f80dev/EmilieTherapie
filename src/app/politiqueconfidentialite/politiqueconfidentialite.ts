1b39c7f1624f87391a27210fcead2d91aced1a60
hhoar <hhoareau@gmail.com>
2026-07-21 09:06:12 +0200

    initial commit

---
+++ src/app/politiqueconfidentialite/politiqueconfidentialite.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-politiqueconfidentialite',
+  imports: [],
+  templateUrl: './politiqueconfidentialite.html',
+  styleUrl: './politiqueconfidentialite.scss',
+})
+export class Politiqueconfidentialite {}
